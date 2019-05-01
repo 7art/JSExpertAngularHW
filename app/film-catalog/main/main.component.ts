0ab510d82a562a29a52b6f0e1688506356761ea5
7art <sem85cv@yandex.ru>
2019-05-01 11:57:37 +0300

    add mat-tab to main comp session 3 hw 2

--- app/film-catalog/main/main.component.ts
+++ app/film-catalog/main/main.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FilmService } from "../film.service";
+import { FilmModel } from "../../film.model";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.css"]
 })
 export class MainComponent implements OnInit {
   pageName: string = "Film Catalog Dashboard";
-
-  constructor() { }
+  films: FilmModel[];
+  constructor(private svc: FilmService) {}
 
   ngOnInit() {
+    this.films = this.svc.getFilms();
   }
-
 }
